7b80232e6efd890bd42ea8d8d735c278fc34cc4f
Diego Borjas <diegoborjas@gmail.com>
2022-10-04 10:43:09 -0500

    made adjustments to deploy app

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -5,6 +6,7 @@ const bodyParser = require('body-parser');
 //Routes
 const usersRoutes = require("./routes/users");
 const teamsRoutes = require("./routes/teams");
+const exp = require('constants');
 
 const app = express();
 
@@ -18,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/sportsBud')
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -35,5 +39,8 @@ app.use((req, res, next) => {
 
 app.use("/api/users", usersRoutes);
 app.use("/api/teams", teamsRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
